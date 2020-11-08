c56a3fa2d7cbe4efc9872fb2a8d0e3d51e54059d
SullFurix <sull.furix@gmail.com>
2020-11-08 01:04:58 +0100

    Hello "username"

---
+++ conf/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  telegram: {
+    apiKey: "YOUR-API-KEY",
+  },
+}
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-#
+require('dotenv').config()
+const config = require("./conf/config.js");
+const TelegramBot = require('node-telegram-bot-api');
+
+const telegram = new TelegramBot(config.telegram.apiKey, {polling: true});
+
+telegram.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  telegram.sendMessage(chatId, resp);
+});
+
+telegram.on('message', (msg) => {
+  console.log(msg);
+  telegram.sendMessage(msg.from.id, `Hello ${msg.chat.username}`);
+});
