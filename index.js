19d68f936aab07418ec7da8389ff55281bbdd017
SullFurix <sull.furix@gmail.com>
2020-11-08 16:37:45 +0100

    Detect language, translate fr to en, en to fr, other language  to fr and en

--- index.js
+++ index.js
@@ -6,8 +6,41 @@ const translate = require('@k3rn31p4nic/google-translate-api');
 const telegram = new TelegramBot(config.telegram.apiKey, {polling: true});
 
 telegram.on('message', (msg) => {
+
   translate(msg.text, { to: 'en' }).then(res => {
-    telegram.sendMessage(msg.from.id, `${msg.chat.username} : `+ res.text);
+
     console.log("language : [" + res.from.language.iso + "] username : [" + `${msg.chat.username}` + "] message : [" + msg.text + "]");
+
+    switch (res.from.language.iso) {
+
+      case 'fr':
+      translate(msg.text, {from: 'fr', to: 'en' }).then(res => {
+        telegram.sendMessage(msg.from.id, "[EN] " + `${msg.chat.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+      break;
+
+      case 'en':
+      translate(msg.text, {from: 'en', to: 'fr' }).then(res => {
+        telegram.sendMessage(msg.from.id, "[FR] " + `${msg.chat.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+      break;
+
+      default:
+      translate(msg.text, { to: 'en' }).then(res => {
+        telegram.sendMessage(msg.from.id, "[EN] " + `${msg.chat.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+
+      translate(msg.text, { to: 'fr' }).then(res => {
+        telegram.sendMessage(msg.from.id, "[FR] " + `${msg.chat.username} : ` + res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
   });
 });
