0447f8bc493a8e8db614b03ea412b4a4546d9652
SullFurix <sull.furix@gmail.com>
2020-11-08 02:29:34 +0100

    Add auto translation

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require('dotenv').config()
 const config = require("./conf/config.js");
 const TelegramBot = require('node-telegram-bot-api');
+const translate = require('@k3rn31p4nic/google-translate-api');
 
 const telegram = new TelegramBot(config.telegram.apiKey, {polling: true});
 
-telegram.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
-  telegram.sendMessage(chatId, resp);
-});
-
 telegram.on('message', (msg) => {
-  console.log(msg);
-  telegram.sendMessage(msg.from.id, `Hello ${msg.chat.username}`);
+  console.log(`${msg.chat.username} : `+msg.text);
+  translate(msg.text, { to: 'en' }).then(res => {
+    telegram.sendMessage(msg.from.id, `${msg.chat.username} : `+res.text);
+  });
 });
