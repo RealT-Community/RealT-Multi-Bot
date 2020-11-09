4d0f632ab09bdeda4c8f506fa6cc4d5894aec364
SullFurix <sull.furix@gmail.com>
2020-11-10 00:12:47 +0100

    Add discord automated transaltion(not tested)

--- conf/config.js
+++ conf/config.js
@@ -2,4 +2,7 @@ module.exports = {
   telegram: {
     apiKey: "YOUR-TELEGRAM-API-KEY",
   },
+  discord: {
+    apiKey: "YOUR-DISCORD-API-KEY",
+  },
 }
--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 require('dotenv').config()
 const config = require("./conf/config.js");
 const TelegramBot = require('node-telegram-bot-api');
+const Discord = require("discord.js");
 const translate = require('@k3rn31p4nic/google-translate-api');
 
 const telegram = new TelegramBot(config.telegram.apiKey, {polling: true});
+const discord = new Discord.Client();
+
+
+discord.on('ready', function() {});
+discord.login(config.discord.apiKey);
+
+
+discord.on('message', (msg) => {
+
+  translate(msg.content, { to: 'en' }).then(res => {
+
+    console.log("language : [" + res.from.language.iso + "] username : [" + `${msg.author.username}` + "] message : [" + msg.content + "]");
+
+    switch (res.from.language.iso) {
+
+      case 'fr':
+      translate(msg.content, {from: 'fr', to: 'en' }).then(res => {
+        msg.channel.send("[EN] " + `${msg.author.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+      break;
+
+      case 'en':
+      translate(msg.content, {from: 'en', to: 'fr' }).then(res => {
+        msg.channel.send("[FR] " + `${msg.author.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+      break;
+
+      default:
+      translate(msg.content, { to: 'en' }).then(res => {
+        msg.channel.send("[EN] " + `${msg.author.username} : `+ res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+
+      translate(msg.content, { to: 'fr' }).then(res => {
+        msg.channel.send("[FR] " + `${msg.author.username} : ` + res.text);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+  });
+});
+
+
 
 telegram.on('message', (msg) => {
 
